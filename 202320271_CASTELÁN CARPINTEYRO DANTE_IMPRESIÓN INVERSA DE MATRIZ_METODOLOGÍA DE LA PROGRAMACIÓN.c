ffd5e24d5a7f4aca152b02496491b8da8da492a4
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-17 12:24:48 -0600

    C

---
+++ 202320271_CASTELÁN CARPINTEYRO DANTE_IMPRESIÓN INVERSA DE MATRIZ_METODOLOGÍA DE LA PROGRAMACIÓN.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int matriz[100][100], n;
+	
+	printf("Por favor, ingrese el valor de n:\n");
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			printf("Por favor, ingrese el valor de [%d][%d]: ", i, j);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	for(int i=(n-1); i>=0; i--){
+		for(int j=(n-1); j>=0; j--){
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
