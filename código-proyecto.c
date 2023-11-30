02ef95c8b7a7fc481992483f06a51bc276380213
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-29 23:18:22 -0600

    Para prueba de registro y visualización de equipos

--- código-proyecto.c
+++ código-proyecto.c
@@ -17,130 +17,7 @@ typedef struct Teams
     Jugadores Players[10];
 } Equipos;
 
-int main(int argc, char *argv[])
-{
-    Equipos Junior[8];
-    Equipos Free[8];
-    int option = 0, opt = 0, equipos = 0;
-    Cadena categories[2];
-    strcpy(categories[0], "junior");
-    strcpy(categories[1], "libre");
-
-    while (option != 10)
-    {
-        printf("Bienvenido, por favor, elija una opcion:\n");
-        printf("1.- Mostrar equipos\n");
-        printf("2.- Registrar equipo\n");
-        printf("3.- Mostrar jugador\n");
-        printf("4.- Registrar jugador\n");
-        printf("5.- Eliminar equipo\n");
-        printf("6.- Eliminar jugador\n");
-
-        printf("10.- Salir\n");
-        scanf("%d", &option);
-        switch (option)
-        {
-        case 1:
-        {
-            int contador = 0;
-            printf("\nEquipos de categoria Junior registrados:\n");
-            for (int i = 0; i < 8; i++)
-            {
-                if (Junior[i].nombre != "")
-                {
-                    contador++;
-                    printf("  %d.- %s\n", contador + 1, Junior[i].nombre);
-                }
-            }
-            contador = 0;
-            printf("\nEquipos de categoria Libre registrados:\n");
-            for (int i = 0; i < 8; i++)
-            {
-                if (Free[i].nombre != "")
-                {
-                    contador++;
-                    printf("  %d.- %s\n", contador + 1, Free[i].nombre);
-                }
-            }
-            break;
-        }
-
-        case 2: // Registrar equipo
-        {
-            opt = 0;
-            int jugadores = 0, birthInfo[3];
-            Cadena team_name = "", name[10][3], tempString;
-
-            while (opt != 1 || opt != 2)
-            {
-                printf("Elija una categoria:\n");
-                printf("1.- Junior\n");
-                printf("2.- Libre\n");
-                scanf("%d", &opt);
-            }
-            printf("Haz elegido la categoria %s.\n", categories[opt - 1]);
-
-            printf("Escribe el nombre del equipo:\n");
-            scanf("%s", &team_name);
-            printf("\n");
-            int i = 0, limitAges[2];
-            int salir = 0;
-            while (jugadores < 6 && salir != 1 && i < 10)
-            {
-                if (jugadores > 5)
-                {
-                    int more = 0;
-                    while (more != 2)
-                    {
-                        printf("¿Quiere agregar otro jugador?\n1.- Sí\n2.- No\n");
-                        scanf(% d, &more);
-                        if (more == 2)
-                        {
-                            printf("Plantel dado de alta.\n");
-                            salir = true;
-                        }
-                    }
-                }
-
-                printf("Añade un jugador.");
-
-                printf("\nIndica el nombre: ");
-                scanf("%s", tempString);
-                strcpy(name[i][0], tempString);
-
-                printf("\nIndica el apellido paterno: ");
-                scanf("%s", tempString);
-                strcpy(name[i][1], tempString);
-
-                printf("\nIndica el apellido paterno: ");
-                scanf("%s", tempString);
-                strcpy(name[i][2], tempString);
-
-                printf("\nIndica el día de nacimiento (DD): ");
-                scanf("%d", &birthInfo[0]);
-
-                printf("\nIndica el mes de nacimiento (MM): ");
-                scanf("%d", &birthInfo[1]);
-
-                printf("\nIndica el año de nacimiento (AAAA): ");
-                scanf("%d", &birthInfo[2]);
-
-                if (opt == 1 || opt == 2)
-                {
-                    registerPlayer(opt, birthInfo, limitAges);
-                    printf("\nJugador registrado exitosamente.\n");
-                }
-                i++;
-            }
-            equipos++;
-            break;
-        }
-        }
-    }
-    printf("\n¡Hasta pronto, organizador!");
-}
-
-int validateAge(int birth[3], int limits[2]) // recibo parámetros como arreglo: día, mes, año y límite inferior - superior de edades en años
+int validateAge(int birth[3], int limits[2]) // recibo parámetros como arreglo: dia, mes, anio y limite inferior - superior de edades en anios
 {
     int accepted = 0;
     int currentDate[3]; // Establezco fecha de comparación de edad en 29/11/2023
@@ -199,7 +76,7 @@ int validateAge(int birth[3], int limits[2]) // recibo parámetros como arreglo:
             }
             else
             {
-                if (currentDate[2] - birth[2] = limits[0])
+                if (currentDate[2] - birth[2] == limits[0])
                 {
                     if (birth[1] < currentDate[1])
                     {
@@ -242,7 +119,7 @@ int validateAge(int birth[3], int limits[2]) // recibo parámetros como arreglo:
     return accepted;
 }
 
-void registerPlayer(int opt, int birthInfo[3], int limitAges[2])
+void registerPlayer(int opt, int birthInfo[3], int limitAges[2], Equipos Junior[8], Equipos Free[8], int equipos, int i, Cadena name[10][3])
 {
     switch (opt)
     {
@@ -250,7 +127,7 @@ void registerPlayer(int opt, int birthInfo[3], int limitAges[2])
     {
         limitAges[0] = 13;
         limitAges[1] = 17;
-        if (validateAges(birthInfo, limitAges) == 1)
+        if (validateAge(birthInfo, limitAges) == 1)
         {
             strcpy(Junior[equipos].Players[i].nombre, name[i][0]);
             strcpy(Junior[equipos].Players[i].apPaterno, name[i][1]);
@@ -265,7 +142,7 @@ void registerPlayer(int opt, int birthInfo[3], int limitAges[2])
     {
         limitAges[0] = 18;
         limitAges[1] = 18;
-        if (validateAges(birthInfo, limitAges) == 1)
+        if (validateAge(birthInfo, limitAges) == 1)
         {
             strcpy(Free[equipos].Players[i].nombre, name[i][0]);
             strcpy(Free[equipos].Players[i].apPaterno, name[i][1]);
@@ -280,4 +157,127 @@ void registerPlayer(int opt, int birthInfo[3], int limitAges[2])
     default:
         printf("Error en datos para registro.");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    Equipos Junior[8];
+    Equipos Free[8];
+    int option = 0, opt = 0, equipos = 0;
+    Cadena categories[2];
+    strcpy(categories[0], "junior");
+    strcpy(categories[1], "libre");
+
+    while (option != 10)
+    {
+        printf("Bienvenido, por favor, elija una opcion:\n");
+        printf("1.- Mostrar equipos\n");
+        printf("2.- Registrar equipo\n");
+        printf("3.- Mostrar jugador\n");
+        printf("4.- Registrar jugador\n");
+        printf("5.- Eliminar equipo\n");
+        printf("6.- Eliminar jugador\n");
+
+        printf("10.- Salir\n");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+        {
+            int contador = 0;
+            printf("\nEquipos de categoria Junior registrados:\n");
+            for (int i = 0; i < 8; i++)
+            {
+                if (Junior[i].nombre != "")
+                {
+                    contador++;
+                    printf("  %d.- %s\n", contador + 1, Junior[i].nombre);
+                }
+            }
+            contador = 0;
+            printf("\nEquipos de categoria Libre registrados:\n");
+            for (int i = 0; i < 8; i++)
+            {
+                if (Free[i].nombre != "")
+                {
+                    contador++;
+                    printf("  %d.- %s\n", contador + 1, Free[i].nombre);
+                }
+            }
+            break;
+        }
+
+        case 2: // Registrar equipo
+        {
+            opt = 0;
+            int jugadores = 0, birthInfo[3];
+            Cadena team_name = "", name[10][3], tempString;
+
+            while (opt != 1 && opt != 2)
+            {
+                printf("Elija una categoria:\n");
+                printf("1.- Junior\n");
+                printf("2.- Libre\n");
+                scanf("%d", &opt);
+            }
+            printf("Haz elegido la categoria %s.\n", categories[opt - 1]);
+
+            printf("Escribe el nombre del equipo:\n");
+            scanf("%s", &team_name);
+            printf("\n");
+            int i = 0, limitAges[2];
+            int salir = 0;
+            while (jugadores < 6 && salir != 1 && i < 10)
+            {
+                if (jugadores > 5)
+                {
+                    int more = 0;
+                    while (more != 2)
+                    {
+                        printf("Quiere agregar otro jugador? \n 1.- Si \n 2.- No \n ");
+                        scanf("%d", &more);
+                        if (more == 2)
+                        {
+                            printf("Plantel dado de alta.\n");
+                            salir = 1;
+                        }
+                    }
+                }
+
+                printf("Aniade un jugador.");
+
+                printf("\nIndica el nombre: ");
+                scanf("%s", tempString);
+                strcpy(name[i][0], tempString);
+
+                printf("\nIndica el apellido paterno: ");
+                scanf("%s", tempString);
+                strcpy(name[i][1], tempString);
+
+                printf("\nIndica el apellido paterno: ");
+                scanf("%s", tempString);
+                strcpy(name[i][2], tempString);
+
+                printf("\nIndica el dia de nacimiento (DD): ");
+                scanf("%d", &birthInfo[0]);
+
+                printf("\nIndica el mes de nacimiento (MM): ");
+                scanf("%d", &birthInfo[1]);
+
+                printf("\nIndica el anio de nacimiento (AAAA): ");
+                scanf("%d", &birthInfo[2]);
+
+                if (opt == 1 || opt == 2)
+                {
+                    registerPlayer(opt, birthInfo, limitAges, Junior, Free, equipos, i, name);
+                    printf("\nJugador registrado exitosamente.\n");
+                }
+                i++;
+            }
+            equipos++;
+            break;
+        }
+        }
+    }
+    printf("\niHasta pronto, organizador!");
 }
